4dfce4113f4de176687116e8da26b665833ad04a
Heiko <heiko.rothermel@yahoo.de>
2022-04-01 01:19:56 +0200

    3

--- TestGitub2/ViewController.swift
+++ TestGitub2/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 
     @IBOutlet var table: UITableView!
-    
-    var models = [Weather]()
+    var models = [Current]()
     
     let locationManager = CLLocationManager()
     
@@ -52,21 +51,59 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         if !locations.isEmpty, currentLocation == nil {
             currentLocation = locations.first
             locationManager.stopUpdatingLocation()
             requestWeatherForLocation()
         }
+        
     }
     
     func requestWeatherForLocation() {
+        
         guard let currentLocation = currentLocation else {
             return
         }
         let long = currentLocation.coordinate.longitude
         let lat = currentLocation.coordinate.latitude
         
-        print("\(long) | \(lat)")
+        let url = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(long)&units=metric&lang=de&appid=7e5da986d80232efd714c8abf2a1db1b"
+        
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+          
+            //Validation
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+            
+            
+            // Convert Data to models/some object
+            var json: WeatherResponse?
+            do {
+                json = try JSONDecoder().decode(WeatherResponse.self, from: data)
+            }
+            catch {
+                print("error: \(error)")
+            }
+            
+            guard let result = json else {
+                return
+            }
+            
+            print(result.current.temp)
+            
+            
+            // Update user interface
+            
+            
+            
+            
+            
+        }).resume()
+        
+        
     }
     
     // Table
@@ -82,7 +119,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 }
 
-struct Weather {
+struct WeatherResponse: Codable {
+    
+//    let lat: Float
+    let lon: Float
+    let timezone: String
+    
+    let current: Current
+    
+    
+    
+}
+
+struct Current: Codable {
+    
+    let temp: Float
+    
+    let weather: Weather
+    
+}
+
+struct Weather: Codable {
+    
+    let main: String
+    
+    
     
 }
 
