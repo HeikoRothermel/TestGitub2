bb0ca274807bad4c7652f43d28a0d6250e5500c3
Heiko <heiko.rothermel@yahoo.de>
2022-03-31 17:51:10 +0200

    Initial Commit

---
+++ TestGitub2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitub2
+//
+//  Created by Heiko Rothermel on 3/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
