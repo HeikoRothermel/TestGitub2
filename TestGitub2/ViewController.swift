a341f2ff7329c5a5fad634ab03b34036244aa9a9
Heiko <heiko.rothermel@yahoo.de>
2022-03-31 21:30:31 +0200

    WeatherApp Step1

--- TestGitub2/ViewController.swift
+++ TestGitub2/ViewController.swift
@@ -7,15 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+    @IBOutlet var table: UITableView!
+    
+    var models = [Weather]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var doesItWork = "YesYesYes"
+        // Register 2 cells
+        
+        table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
+        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        
     }
+    
+    
+    // Table
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
+}
+
+struct Weather {
+    
 }
 
